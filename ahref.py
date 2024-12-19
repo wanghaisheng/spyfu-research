b51b92e44b406c3ed039f28c4009cf935b35b6a1
HeisenBerg? <admin@tiktokastudio.com>
2024-12-20 02:17:21 +0800

    Update ahref.py

--- ahref.py
+++ ahref.py
@@ -1,8 +1,8 @@
 from DrissionPage import ChromiumOptions, ChromiumPage
-co = ChromiumOptions().headless()
+# co = ChromiumOptions().headless()
+# browser = Chromium(co)
 
-def getahrefkd(k):
-    browser = Chromium(co)
+def getahrefkd(k,browser):
 
     url = "https://ahrefs.com/keyword-difficulty/"
     browser.get(url)
@@ -24,18 +24,26 @@ def getahrefkd(k):
 def getahrefsv(k):
     browser = Chromium(co)
 
-    url = "https://ahrefs.com/keyword-difficulty/"
+    url = "https://ahrefs.com/keyword-generator/"
     browser.get(url)
     # keyword = "remini.ai"
     browser.ele("@placeholder=Enter keyword").input(keyword)
 
     # 点击登录按钮
     browser.ele("text=Check keyword").click()
-    kd = browser.ele(".css-16bvajg-chartValue").text
+    svs=browser.ele('t:table').eles('t:tr')
+    results=[]
+    for line in eles:
+       cols= line.eles('t:td'):
+       k=cols[0].text
+       kd=cols[1].text
+       volume=cols[2].text
+       results.append({
 
-    kds = browser.ele(".css-1wi5h2-row css-1crciv5 css-6rbp9c").text
-    #     print(kd)
-    #     print(kds)
+           "keyowrd":k,
+           "kdlevel":kd,
+           volume:volume
+       }) 
 
-    return {"keyword": keyword, "kd": kd, "des": kds}
+    return {"data":results }
 
