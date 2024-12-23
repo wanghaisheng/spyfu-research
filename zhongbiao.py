6b0460eb91b39566cdf12addaad890fe8290987c
HeisenBerg? <admin@tiktokastudio.com>
2024-12-24 04:50:36 +0800

    Update zhongbiao.py

--- zhongbiao.py
+++ zhongbiao.py
@@ -44,12 +44,13 @@ def md2json(md):
         print(response["choices"][0]["message"]["content"].strip())
 
 
-def getpagecount():
+def getpagecount(keyword=None):
     print("borw", browser)
     tab = browser.new_tab()
+    
     domain = "https://search.ccgp.gov.cn/bxsearch?searchtype=2&page_index=1&bidSort=0&buyerName=&projectId=&pinMu=0&bidType=7&dbselect=bidx&kw=CT&start_time=2024%3A06%3A25&end_time=2024%3A12%3A24&timeType=5&displayZone=&zoneId=&pppStatus=0&agentName="
 
-    tab.get(url)
+    tab.get(domain)
     print("sssss")
     counts = tab.ele(".pager").eles("tag:a")[-2].text
     print(counts)
